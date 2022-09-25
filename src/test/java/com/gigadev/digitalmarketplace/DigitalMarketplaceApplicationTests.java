d464c509380040e3fbe0be867ffbc359bc1f3f83
gi-ga-dev <cloud.gianluca@gmail.com>
2022-09-25 15:31:31 +0200

    first commit

---
+++ src/test/java/com/gigadev/digitalmarketplace/DigitalMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigadev.digitalmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
