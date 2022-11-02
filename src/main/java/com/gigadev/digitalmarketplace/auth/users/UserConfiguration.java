92b0fd915a202138af5e19b30526ec64e87c2f89
gi-ga-dev <cloud.gianluca@gmail.com>
2022-11-02 17:10:40 +0100

    final merge be

--- src/main/java/com/gigadev/digitalmarketplace/auth/users/UserConfiguration.java
+++ src/main/java/com/gigadev/digitalmarketplace/auth/users/UserConfiguration.java
@@ -2,7 +2,6 @@ package com.gigadev.digitalmarketplace.auth.users;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class UserConfiguration {
