b8b78e759e9dec4f487802b66ac1baeba7ff9cd7
gi-ga-dev <cloud.gianluca@gmail.com>
2022-11-14 23:57:47 +0100

    Scope prototype test

--- src/main/java/com/gigadev/digitalmarketplace/auth/roles/RoleConfiguration.java
+++ src/main/java/com/gigadev/digitalmarketplace/auth/roles/RoleConfiguration.java
@@ -2,16 +2,19 @@ package com.gigadev.digitalmarketplace.auth.roles;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class RoleConfiguration {
 	
 	@Bean("roleAdmin")
+	@Scope("prototype")
 	public Role roleAdmin() {
 		return new Role(ERole.ROLE_ADMIN);
 	}
 	
 	@Bean("roleUser")
+	@Scope("prototype")
 	public Role roleUser() {
 		return new Role(ERole.ROLE_USER);
 	}
